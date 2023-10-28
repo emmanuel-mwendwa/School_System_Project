662611344f9913d9cd13a7574a958639ba3382e6
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-10-28 11:08:19 +0300

    README file explaining branch protection rules

---
+++ README.md
@@ -0,0 +1,6 @@
+The main branch has a protection rule that avoids pushing to it directly.
+You will need to create your own branch.
+After "git push" you can ask for a "pull request" to merge to the main branch.
+A team member will review the changes made to the project and allow the merge.
+This will prevent a case where the system main branch crashes.
+For further clarification "https://youtu.be/rYwwz1b2Nss"
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.school_system"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.school_system"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -36,6 +37,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-firestore:24.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
